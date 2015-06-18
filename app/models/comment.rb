87f8e9569eacebbb15f677af93ac41ae9d5a29a9
Danelson Rosa <danelson.rosa@gmail.com>
2015-06-18 11:31:21 -0400

    Added validations to both recipe and comment classes.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :recipe
+  validates :comment, numericality: { less_than_or_equal_to: 140 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_many :comments
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  #validates :name, inclusion: { in: "/brussel sprouts/i" }
 end
